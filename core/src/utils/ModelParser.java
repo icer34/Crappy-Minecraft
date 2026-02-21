bf7915b84ae606f50e0ce187cb8d9e28522447ed
Desoleil Thomas <Thomas.Desoleil@student.uliege.be>
2026-02-21 17:39:51 +0100

    finished the obj format for the model parser

--- core/src/utils/ModelParser.java
+++ core/src/utils/ModelParser.java
@@ -8,14 +8,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public final class ModelParser {
     private ModelParser(){}
 
-    private record Vertex (float[] pos, float[] norm, float[] uv) {}
+    private record Vertex (float[] pos, float[] norm, float[] uv, int idx) {}
 
-    public MeshData parseOBJ(String path) {
+    public static MeshData parseOBJ(String path) {
         List<String> lines;
         try {
             lines = Files.readAllLines(Path.of(path));
@@ -27,6 +28,7 @@ public final class ModelParser {
         ArrayList<float[]> positions = new ArrayList<>();
         ArrayList<float[]> normals = new ArrayList<>();
         ArrayList<float[]> uvs = new ArrayList<>();
+        ArrayList<String> faceData = new ArrayList<>();
 
         for (String l : lines) {
             String[] parts = l.split(" ");
@@ -51,12 +53,45 @@ public final class ModelParser {
                                         Float.parseFloat(parts[2])});
                 }
 
+                case "f" -> {
+                    faceData.add(parts[1]);
+                    faceData.add(parts[2]);
+                    faceData.add(parts[3]);
+                }
+
                 default -> {
                 }
             }
         }
 
         //from the face data, keep only once the vertices needed and then add to the final data
-        //TODO
+        //map that keeps the known vertices, key = the vertex in the obj format (pos/tex/norm), val = Vertex(pos, norm, uv)
+        HashMap<String, Vertex> knownVertices = new HashMap<>();
+        FloatArrayList finalVertData = new FloatArrayList();
+        IntArrayList finalIdxData = new IntArrayList();
+        int numVertices = 0;
+
+        for(String vert : faceData) {
+            if(knownVertices.containsKey(vert)) {
+                Vertex v = knownVertices.get(vert);
+                finalIdxData.add(v.idx);
+            } else {
+                String[] vertexIdx = vert.split("/");
+                Vertex v = new Vertex(positions.get(Integer.parseInt(vertexIdx[0]) - 1),
+                                      normals.get(Integer.parseInt(vertexIdx[2]) - 1),
+                                      uvs.get(Integer.parseInt(vertexIdx[1]) - 1),
+                                      numVertices++);
+
+                knownVertices.put(vert, v);
+
+                finalVertData.add(v.pos[0]); finalVertData.add(v.pos[1]); finalVertData.add(v.pos[2]);
+                finalVertData.add(v.norm[0]); finalVertData.add(v.norm[1]); finalVertData.add(v.norm[2]);
+                finalVertData.add(v.uv[0]); finalVertData.add(v.uv[1]);
+
+                finalIdxData.add(v.idx);
+            }
+        }
+
+        return new MeshData(finalVertData.toFloatArray(), finalIdxData.toIntArray());
     }
 }
